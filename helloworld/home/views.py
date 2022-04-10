ad1424c65bcecae92b26f72fb343d2bcfa4d5988
priyam <priyamshankar.5@gmail.com>
2022-04-10 17:30:45 +0530

    dynamic changes using context

--- helloworld/home/views.py
+++ helloworld/home/views.py
@@ -1,21 +1,24 @@
-from django.http import HttpRequest
 from django.shortcuts import render, HttpResponse
-# from django.views.generic.edit  import CreateView
+# from matplotlib.style import context
 
 # Create your views here.
 
 
 def index(request):
-    # return HttpResponse("this is a homepage")
-    return render(request,"index.html")
+    context ={
+        "vary" : "home variable"
+    }
+    return render(request, "index.html",context)
+    # return HttpResponse("this is a home page")
 
 # def about2(request):
 
 
 def about(request):
     # return HttpResponse("this is a priyam page")
-    return render(request,"about.html")
+    return render(request, "about.html")
+
 
 def contact(request):
     # return HttpResponse("this is a contact us page")
-    return render(request,"contact.html")
+    return render(request, "contact.html")
