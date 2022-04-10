8b2ef363db1f0316fda9077a2cb2cb62831e379d
priyam <priyamshankar.5@gmail.com>
2022-04-10 16:44:49 +0530

    render pages

--- helloworld/home/views.py
+++ helloworld/home/views.py
@@ -6,13 +6,16 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse("this is a homepage")
+    # return HttpResponse("this is a homepage")
+    return render(request,"index.html")
 
 # def about2(request):
 
 
 def about(request):
-    return HttpResponse("this is a priyam page")
+    # return HttpResponse("this is a priyam page")
+    return render(request,"about.html")
 
 def contact(request):
-    return HttpResponse("this is a contact us page")
+    # return HttpResponse("this is a contact us page")
+    return render(request,"contact.html")
