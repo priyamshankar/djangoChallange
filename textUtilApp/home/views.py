ea00594c1cbe11dc07fbbf1c93affe42ca48ecdc
priyam <priyamshankar.5@gmail.com>
2022-04-11 23:45:09 +0530

    basics settings setup

--- textUtilApp/home/views.py
+++ textUtilApp/home/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from urllib import response
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def index(request):
+    # return HttpResponse("this is a home page")
+    return render(request,"index.html")
