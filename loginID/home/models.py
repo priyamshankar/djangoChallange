03fc30bd8f306bc999c77f7aaccd69da996fffa4
priyam <priyamshankar.5@gmail.com>
2022-04-14 01:30:46 +0530

    slight modifications

--- loginID/home/models.py
+++ loginID/home/models.py
@@ -9,5 +9,5 @@ class Destination(models.Model):
     name= models.CharField(max_length=100)
     # img= models.ImageField(upload_to='pics')
     desc= models.TextField()
-    price= models.IntegerField
+    price= models.IntegerField()
     offer= models.BooleanField(default=false)
