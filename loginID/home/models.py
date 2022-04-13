49e3da50f1f770e4f1dce4e651ab19c7bf715934
priyam <priyamshankar.5@gmail.com>
2022-04-13 23:19:30 +0530

    creating the models in home app

--- loginID/home/models.py
+++ loginID/home/models.py
@@ -1,3 +1,13 @@
+from distutils.command.upload import upload
 from django.db import models
+from sqlalchemy import false
 
 # Create your models here.
+
+
+class Destination(models.Model):
+    name= models.CharField(max_length=100)
+    # img= models.ImageField(upload_to='pics')
+    desc= models.TextField()
+    price= models.IntegerField
+    offer= models.BooleanField(default=false)
--- loginID/loginId/settings.py
+++ loginID/loginId/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'loginId.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'priyamDB',
+        'user': 'postgres',
+        'password':'12345',
+        'HOST': 'localhost'
     }
 }
 
