f95fa2e132f80e38b442efd5c3a4a09210df1a10
priyam <priyamshankar.5@gmail.com>
2022-04-14 16:47:59 +0530

    added the model for the loginModle

--- loginID/home/models.py
+++ loginID/home/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+import email
 from django.db import models
 from sqlalchemy import false
 
@@ -11,3 +12,11 @@ class Destination(models.Model):
     desc= models.TextField()
     price= models.IntegerField()
     offer= models.BooleanField(default=false)
+
+class loginModel(models.Model):
+    userName=models.CharField(max_length=50)
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=70)
+    password=models.CharField(max_length=30)
+    manager=models.BooleanField(default=false)
+    
